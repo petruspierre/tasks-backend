bbb824ed0479f1c11c370556ecfee38555edd22a
Petrus Pierre <petrusfla@gmail.com>
2020-01-31 13:58:09 -0300

    Sei la

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const db = require('./config/db')
 const consign = require('consign')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors({
+    origin: '*'
+}))
 
 consign()
     .include('./config/passport.js')
-    .then('./config/middlewares.js')
     .then('./api')
     .then('./config/routes.js')
     .into(app)
